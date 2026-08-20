37354b8ff704489eb35b43277fd66259a872f2af
Saqib Ansari <nextchamp.saqib@gmail.com>
2026-08-20 12:47:57 +0530

    chore(desk): stop loading the insights nudge on desk
    (cherry picked from commit b164526390e3d6775e3ae9df0c1ba123941471cd)

--- insights/hooks.py
+++ insights/hooks.py
@@ -42,7 +42,7 @@ ui_islands = {
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/insights/css/insights.css"
-app_include_js = "insights_nudge.bundle.js"
+# app_include_js = "insights_nudge.bundle.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/insights/css/insights.css"
